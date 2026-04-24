20450709a823c00fc25daa98875898ecb493b4eb
chahin5-hub <sebaachahin3@gmail.com>
2026-04-24 21:40:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
-const elements = document.querySelectorAll('.hidden');
+// Typing
+const text = "شـاهـيـن";
+let i = 0;
+const el = document.getElementById("name");
 
-window.addEventListener('scroll', () => {
-    elements.forEach(el => {
-        const top = el.getBoundingClientRect().top;
-        if (top < window.innerHeight - 100) {
-            el.classList.add('show');
+function typing(){
+    if(i < text.length){
+        el.innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typing,120);
+    }
+}
+typing();
+
+// Scroll animation
+const elements = document.querySelectorAll('.section');
+window.addEventListener('scroll',()=>{
+    elements.forEach(el=>{
+        if(el.getBoundingClientRect().top < window.innerHeight - 100){
+            el.style.opacity = 1;
+            el.style.transform = "translateY(0)";
         }
     });
 });
+
+
